40643640e48577bce501ee3c7618b92e9ff58362
farhanhariss <farhanharis66@gmail.com>
2023-03-23 09:43:03 +0700

    Initial Commit

---
+++ app/src/main/java/com/farhanharis/githubuser/ApiConfig.kt
@@ -0,0 +1,25 @@
+package com.farhanharis.githubuser
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiConfig {
+    companion object{
+        fun getApiService(): ApiService {
+            val loggingInterceptor =
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                // Anda juga dapat menggunakan class ini untuk keperluan API lain dengan mengganti kode pada base URL dan nama service-nya
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+}
